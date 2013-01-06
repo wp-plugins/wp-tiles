a275dc1d4350a76dd2bb23b61475bf734ba2c428
Mike_Cowobo <Mike_Cowobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-06 15:59:02 +0000

    Updating tag to 0.2.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-tiles/tags/0.2.1@648764 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/wp-tiles-defaults.php
@@ -0,0 +1,69 @@
+<?php
+global $wptiles_defaults;
+$wptiles_defaults = array(
+                // Post query
+                "posts_query"         => array (
+                                    'numberposts'   => 20,
+                                    'offset'        => 0,
+                                    'category'      => 0,
+                                    'orderby'       => 'post_date',
+                                    'order'         => 'DESC',
+                                    'include'       => array(),
+                                    'exclude'       => array(),
+                                    'meta_key'      => '',
+                                    'meta_value'    =>'',
+                                    'post_type'     => 'post',
+                                    'suppress_filters'
+                                                    => true,
+                ),
+                'colors'        => array (
+                    "colors"        => array (
+                                    "#009999",
+                                    "#1D7373",
+                                    "#006363",
+                                    "#33CCCC",
+                                    "#5CCCCC",
+                        ),
+                    ),
+                'templates'     => array (
+                    'show_selector' => 'true',
+                    "templates"     => array (
+                                  "News" => " A A B B . \n"
+                                          . " A A . C C \n"
+                                          . " . D D E E \n"
+                                          . " F F . E E ",
+
+                           "Alternative" => " . A A B B \n"
+                                          . " . A A C C \n"
+                                          . " D D F F . \n"
+                                          . " E E F F . ",
+
+                                "Banner" => " A . B . C \n"
+                                          . " A . B . C \n"
+                                          . " . D . E . \n"
+                                          . " . D . E . ",
+
+                              "Featured" => " . A A A . \n"
+                                          . " . A A A . \n"
+                                          . " . A A A . ",
+
+                                 "Plain" => " . . . . . ",
+
+                             "Condensed" => " . . . . . . . . ",
+
+                                    ),
+                    "small_screen_template" =>
+                                          " A A \n"
+                                        . " . . \n"
+                                        . " A A \n"
+                                        . " . . \n"
+                                        . " A A \n"
+                                        . " . . \n"
+                                        . " A A "
+                    ,
+                ),
+
+                // These are only for in the shortcodes
+                "template"  => '',
+                'show_selector' => '',
+            );
