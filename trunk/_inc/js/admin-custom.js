c30747c0389ba3047d453307ad8743638c0e9ea2
Mike_Cowobo <Mike_Cowobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-11 14:31:07 +0000

    Update to version 0.3.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-tiles/tags/0.2.1@651328 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/_inc/js/admin-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready( function () {
-    var colorpickers = jQuery('.wp-tiles-colorpickers');
-    jQuery.each(colorpickers, function ( i, value ) {
-        jQuery(this).farbtastic("#wptiles-color-" + i );
-    });
-});
