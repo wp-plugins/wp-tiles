375bc295307bd0f99fdaf35ff25b60cc05d84299
Mike_Cowobo <Mike_Cowobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-24 04:54:08 +0000

    Updating to 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-tiles/tags/1.0.1@1097840 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/views/about.php
@@ -0,0 +1,75 @@
+<div class='wp-tiles-stuffbox stuffbox' id="about-wp-tiles">
+
+    <h3>About WP Tiles</h3>
+
+    <div class='inside'>
+
+        <p>Thank you for using WP Tiles. If you are ever stuck using the plugin you can check out the detailed documentation and example pages on <a href='http://wp-tiles.com/' target='_blank'>wp-tiles.com</a>.
+            If you need support, you can head over to the <a href='https://wordpress.org/support/plugin/wp-tiles' target='_blank'>support forums</a> on WordPress.org.</p>
+
+        <p>-- Mike</p>
+
+        <a href='https://wordpress.org/support/profile/mike_cowobo' target='_blank'>
+            <img alt='Mike' src='http://1.gravatar.com/avatar/b2e3ffa705cde285c2fb9ce596c85126?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G' class='avatar avatar-96 photo' height='96' width='96' />
+        </a><br />
+
+    </div>
+
+    <hr />
+
+    <h3>Become a Remaintenance Affiliate</h3>
+
+    <div class='inside'>
+
+        <p>Our (awesome) WordPress maintenance service is now offering an affiliate program with <strong>massive pay-outs</strong>.</p>
+
+        <p>For every referral, you get <strong>€150</strong>. And if your referral pays a year in advance, we'll double it for a total of <strong>€300</strong>. Easy money!</p>
+
+        <p>Read how you can become an affiliate on <a href='https://remaintenance.io/affiliate-program/' target='_blank'>our website</a>.</p>
+
+
+    </div>
+
+    <hr />
+
+    <h3>TiledFourteen</h3>
+    <span class='subtitle'>Our free WP Tiles theme</span>
+
+    <div class='inside'>
+
+        <p>Did you know that we also have a free theme especially for WP Tiles based websites?
+        It's a child theme of Twenty Fourteen.
+        You can see it in action on the demo pages on <a href='http://wp-tiles.com' target='_blank'>our website</a>.
+        Check out the theme page <a href='http://wp-tiles.com/tiledfourteen/' target='_blank'>here</a>!</p>
+
+    </div>
+
+    <hr />
+
+    <h3>Stay up to date</h3>
+
+    <div class='inside'>
+
+        To stay up to date with the development of WP Tiles, please sign up to our newsletter below:</p>
+
+        <!-- Begin MailChimp Signup Form -->
+        <div id="mc_embed_signup">
+            <form action="http://trenvo.us2.list-manage.com/subscribe/post?u=999667a0c77440ad37655daa6&amp;id=7b9dc606b6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div class="mc-field-group">
+                    <input type="email" value="<?php echo wp_get_current_user()->user_email ?>" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+                    <input type="hidden" value="<?php site_url() ?>" name="SITE" class=" url" id="mce-SITE"><br /><br />
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button-primary">
+                </div>
+
+                <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;"><input type="text" name="b_999667a0c77440ad37655daa6_7b9dc606b6" tabindex="-1" value=""></div>
+            </form>
+        </div>
+        <!--End mc_embed_signup-->
+
+    </div>
+
+</div>
